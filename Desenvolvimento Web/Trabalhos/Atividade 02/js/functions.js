551e348a3a7c7f52c1dee8f8cdf54906d1427e05
João Lucas Oliveira Vieira <cdcjoaolucas@gmail.com>
2022-05-26 01:34:11 -0300

    New Atividade JS

---
+++ Desenvolvimento Web/Trabalhos/Atividade 02/js/functions.js
@@ -0,0 +1,46 @@
+function atividade01 () {
+    var n1 = Number(window.prompt("Informe o primeiro número."));
+    var n2 = Number(window.prompt("Informe o segundo número."));
+
+    if (n1 > n2) {
+        document.getElementById("exit").innerHTML = "Maior: " + n1 + "<br>Menor: " + n2;
+    }   else {
+        document.getElementById("exit").innerHTML = "Maior: " + n2 + "<br>Menor: " + n1;
+    }
+}
+
+function atividade02 () {
+    
+}
+
+function atividade03 () {
+    
+}
+
+function atividade04 () {
+    
+}
+
+function atividade05 () {
+    
+}
+
+function atividade06 () {
+    
+}
+
+function atividade07 () {
+    
+}
+
+function atividade08 () {
+    
+}
+
+function atividade09 () {
+    
+}
+
+function atividade10 () {
+    
+}
