055a253a2d0f78f6629b451836a161cb7183db8c
João Lucas Oliveira Vieira <cdcjoaolucas@gmail.com>
2022-06-20 12:23:57 -0300

    Agenda Java update

--- Desenvolvimento de Software/Trabalhos/Agenda/src/Address.java
+++ Desenvolvimento de Software/Trabalhos/Agenda/src/Address.java
@@ -1,10 +1,68 @@
 public class Address {
     
-    String logradouro;
-    int numero;
-    String complemento;
-    String bairro;
-    String cep;
-    String cidade;
+    // Em português, porque ingles só complicou.
+    String log; // Logradouro
+    int num; // Número
+    String cmp; // Complemento
+    String brr; // Bairro
+    String cep; // CEP
+    String cdd; // Cidade
+    
+    protected Address(String log, int num, String cmp, String brr, String cep, String cdd) {
+        this.log = log;
+        this.num = num;
+        this.cmp = cmp;
+        this.brr = brr;
+        this.cep = cep;
+        this.cdd = cdd;
+    }
 
+    // Getters e Setters Geral
+    public String getLog() {
+        return log;
+    }
+
+    public void setLog(String log) {
+        this.log = log;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getCmp() {
+        return cmp;
+    }
+
+    public void setCmp(String cmp) {
+        this.cmp = cmp;
+    }
+
+    public String getBrr() {
+        return brr;
+    }
+
+    public void setBrr(String brr) {
+        this.brr = brr;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCdd() {
+        return cdd;
+    }
+
+    public void setCdd(String cdd) {
+        this.cdd = cdd;
+    }
 }
--- Desenvolvimento de Software/Trabalhos/Agenda/src/CtBook.java
+++ Desenvolvimento de Software/Trabalhos/Agenda/src/CtBook.java
@@ -1,4 +1,6 @@
 
+import java.util.HashMap.*;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -32,21 +34,21 @@ public class CtBook {
             scanner.nextLine();
     }
 
-    public static void main(String[] args) throws Exception{
+    int index = 0;
+    Contact[] contato = new Contact[100];
+
+    public void main(String[] args) throws Exception{
 
         Scanner input = new Scanner(System.in);
 
         int op = 0;
-        int index = 0;
-        int id = 0;
-        int i = 0;
-    
+
         do {
             clearConsole();
             System.out.printf("====== Agenda de Contatos ======\n");
             System.out.printf("\n       ------ MENU ------\n\n");
             System.out.printf(" [ 1 ] Cadastrar Contato \n");
-            System.out.printf(" [ 2 ] Consultar Contato \n");
+            System.out.printf(" [ 2 ] Alterar Contato \n");
             System.out.printf(" [ 3 ] Listar os Contatos \n");
             System.out.printf(" [ 4 ] Excluir um Contato \n");
             System.out.printf(" [ 5 ] Sair \n");
@@ -57,35 +59,16 @@ public class CtBook {
     
             switch (op) {
             case 1:
-                newContato(&contato[index]);
-                index++;
+                newContact();
                 break;
             case 2:
-                searchContato(contato, index);
+                altContact();
                 break;
             case 3:
-                listContato(contato, index);
+                listContact();
                 break;
             case 4:
-                clearConsole();
-                System.out.printf("\n------ Excluir Contato ------\n");
-                System.out.printf("\nInforme o Código: ");
-                input.nextInt(id);
-                System.out.printf("\n\n Código: %d", id);
-                System.out.printf("\n Nome: %s", contato[id].nome);
-                System.out.printf("\n Telefone: %s", contato[id].tel);
-                System.out.printf("\n Email: %s", contato[id].email);
-                System.out.printf("\n\nDeseja Excluir o Contato?\n [ 1 ] Sim\n [ 2 ] N�o\n\nInforme uma Op��o: ");
-    
-                input.nextInt(i);
-                if (i == 1) {
-                    for (i = id; i < index; i++) {
-                        strcpy(contato[i].nome, contato[i + 1].nome);
-                        strcpy(contato[i].tel, contato[i + 1].tel);
-                        strcpy(contato[i].email, contato[i + 1].email);
-                    }
-                    index--;
-                }
+                delContact();
                 break;
             case 5:
                 clearConsole();
@@ -96,4 +79,52 @@ public class CtBook {
             }
         } while (op != 5);
     }
+
+    public void newContact(){
+        Scanner input = new Scanner(System.in);
+        clearConsole();
+        System.out.printf("\n------ Cadastrar Contato ------\n");
+        System.out.printf("Nome: ");
+        System.out.printf("RG: ");
+        System.out.printf("Data de Nascimento: ");
+        System.out.printf("---- E-mail ----");
+        System.out.printf("Principal: "); 
+        System.out.printf("Secundário: ");
+        System.out.printf("---- Telefone ----");
+        System.out.printf("Residencial: ");
+        System.out.printf("Comercial: ");
+        System.out.printf("Celular: ");
+        System.out.printf("---- Endereço ----");
+        System.out.printf("Residencial: ");
+        System.out.printf("Comercial");
+    }
+
+    private void altContact(){
+        clearConsole();
+        System.out.printf("\n------ Alterar Contato ------\n");
+        System.out.printf("Nome: ");
+        System.out.printf("RG: ");
+        System.out.printf("Data de Nascimento: ");
+        System.out.printf("---- E-mail ----");
+        System.out.printf("Principal: "); 
+        System.out.printf("Secundário: ");
+        System.out.printf("---- Telefone ----");
+        System.out.printf("Residencial: ");
+        System.out.printf("Comercial: ");
+        System.out.printf("Celular: ");
+        System.out.printf("---- Endereço ----");
+        System.out.printf("Residencial: ");
+        System.out.printf("Comercial");
+    }
+
+    private void listContact(){
+        clearConsole();
+        System.out.printf("\n------ Listar Contatos ------\n");
+    }
+
+    private void delContact(){
+        clearConsole();
+        System.out.printf("\n------ Excluir Contato ------\n");
+    }
+
 }
